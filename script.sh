9ae6581e68fcec5539b704aa2bf8f1b2b3de66a3
Magnus Fagertun <magnusfagertun@google.com>
2021-05-30 15:03:44 +0000

    changed to cat instead of cp for secret

--- script.sh
+++ script.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-#wait and retry logic
-for i in $(seq 1 5); do cp /secret/.df-credentials.json . && s=0 && break || s=$? && sleep 5; done; (exit $s)
+cat /secret/.df-credentials.json > .df-credentials.json
 dataform run
